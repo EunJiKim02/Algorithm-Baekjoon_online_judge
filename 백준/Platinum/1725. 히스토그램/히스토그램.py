c3a7907f46df765e8d41bc44b25b66fa8d25df29
Mobuk <87495422+mobuktodae@users.noreply.github.com>
2023-05-17 14:20:04 +0900

    [Platinum V] Title: 히스토그램, Time: 512 ms, Memory: 35108 KB -BaekjoonHub

---
+++ 백준/Platinum/1725. 히스토그램/히스토그램.py
@@ -0,0 +1,76 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+INF = 999999999999999999
+
+# MinimumCross 한번 더 생각해보기..
+# 분명 다 옴. 5 1 2 3 4 5 케이스가 안됨
+# 중간을 분할해서 판단할 수 없음. 이걸 새로 짜야함. 
+def MinimumCross(arr, start, mid, end):
+    n = end - start
+    lstart = mid
+    lend = mid
+    result = [0,0]
+    minn = arr[mid]
+    for _ in range(n):
+        if lstart <= start: # lend 이동
+            lend += 1
+            if arr[lend] < minn:
+                minn = arr[lend]
+            if result[0] * result[1] < minn * (lend - lstart + 1):
+                result = (minn , (lend - lstart + 1))
+        elif lend >= end: # lstart 이동
+            lstart -= 1
+            if arr[lstart] < minn:
+                minn = arr[lstart]
+            if result[0] * result[1] < minn * (lend - lstart + 1):
+                result = (minn , (lend - lstart + 1))
+        else : # 둘 중에 큰 거로 이동
+            if arr[lend +1] > arr[lstart -1]:
+                lend += 1
+                if arr[lend] < minn:
+                    minn = arr[lend]
+                if result[0] * result[1] < minn * (lend - lstart + 1):
+                    result = (minn , (lend - lstart + 1))
+            else:
+                lstart -= 1
+                if arr[lstart] < minn:
+                    minn = arr[lstart]
+                if result[0] * result[1] < minn * (lend - lstart + 1):
+                    result = (minn , (lend - lstart + 1))
+
+    return result
+
+            
+    
+
+def MinimumSquare(arr, start, end):
+    if end == start:
+        #print(start, end)
+        return (arr[start], end - start + 1)
+    mid = (start + end) // 2
+    leftmax, leftn = MinimumSquare(arr, start, mid)
+    rightmax, rightn = MinimumSquare(arr, mid+1, end)
+    crossmax, crossn = MinimumCross(arr, start, mid, end)
+    #print(start, end , ':', leftmax , leftn, rightmax , rightn, crossmax , crossn)
+    if (crossmax * crossn <= rightmax * rightn) and (leftmax * leftn <= rightmax * rightn) :
+        return (rightmax , rightn)
+    elif (crossmax * crossn <= leftmax * leftn) and (rightmax * rightn <= leftmax * leftn) :
+        return (leftmax , leftn)
+    else:
+        return (crossmax , crossn)
+    
+    
+
+def main():
+    n = int(input())
+    arr = []
+    for _ in range(n):
+        arr.append(int(input()))    
+    m, n = MinimumSquare(arr, 0, n-1)
+    print(m * n)
+
+
+if __name__ == '__main__':
+    main()
