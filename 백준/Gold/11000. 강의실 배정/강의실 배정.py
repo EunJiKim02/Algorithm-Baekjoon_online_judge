f623539350909a02c94afc05fd20a335e6b2dc33
Mobuk <87495422+mobuktodae@users.noreply.github.com>
2023-06-13 19:40:34 +0900

    [Gold V] Title: 강의실 배정, Time: 376 ms, Memory: 64368 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,28 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+pq = []
+ans = []
+arr = []
+
+N = int(input())
+pq = []
+
+for i in range(N):
+    s, f = map(int, input().split())
+    arr.append([s, f])
+
+arr.sort()
+heapq.heappush(pq, arr[0][1])
+
+for i in range(1, N):
+    if pq[0] > arr[i][0]:
+        heapq.heappush(pq,arr[i][1])
+    else:
+        heapq.heappop(pq)
+        heapq.heappush(pq,arr[i][1])
+       
+
+print(len(pq))
